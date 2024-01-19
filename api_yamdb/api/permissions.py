0e83a31bc59f0aeeb8acf9e16c97137cabcd2ec9
RavenIV <torrans5@yandex.ru>
2024-01-19 17:08:21 +0300

    Исправлен IsAdminOrReadOnly

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -17,7 +17,8 @@ class IsAdminOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user.role == 'admin'
+            or request.user.is_authenticated
+            and request.user.role == 'admin'
         )
 
 
