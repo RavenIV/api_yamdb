3f12b41716208d48d8d1447398203ccf8e4d1a19
RavenIV <torrans5@yandex.ru>
2024-01-20 10:05:04 +0300

    Update import_csv

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -42,7 +42,7 @@ def populate_model(reader, model, fields):
 
 
 class Command(BaseCommand):
-    help = 'Import csv files into database'
+    help = 'Имортировать данные файлов .csv из папки ./static/data/ в базу данных.'
 
     def handle(self, *args, **options):
         for file_name, model, fields in FILE_MODEL_FIELDS:
